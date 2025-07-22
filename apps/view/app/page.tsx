822e7ff482579117fb8e24ad2a88efca85a22bae
gervickasjs <wellitogervickas@gmail.com>
2025-07-22 16:52:12 -0300

    feat: add initial tauri invoke

--- apps/view/app/page.tsx
+++ apps/view/app/page.tsx
@@ -1,10 +1,38 @@
+"use client";
+import { invoke } from "@tauri-apps/api/core";
+import { useState } from "react";
+
 import { Card } from "@repo/ui/components/card";
 
 export default function Home() {
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+    setGreetMsg(await invoke("greet", { name }));
+  }
+
   return (
     <div>
       <div>
-        <Card title="My Title">My Card</Card>
+        <Card title="My Title">
+          <form
+            className="row"
+            onSubmit={(e) => {
+              e.preventDefault();
+              greet();
+            }}
+          >
+            <input
+              id="greet-input"
+              onChange={(e) => setName(e.currentTarget.value)}
+              placeholder="Enter a name..."
+            />
+            <button type="submit">Greet</button>
+          </form>
+          <p>{greetMsg}</p>
+        </Card>
       </div>
     </div>
   );
